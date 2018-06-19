9f33c69a0835d3cda70971e7418a5001ae5ede2a
Dimitar Nanov <dimitar.nanov@ecollect.net>
2018-06-19 09:37:09 +0300

    fix some erros by sagas

--- saga/builder.js
+++ saga/builder.js
@@ -5,9 +5,9 @@ const sagaBuilder = require('./builders/sagaBuilder');
 const loader = require('./loader');
 
 const buildSagas = (collections, definitions) => Object.entries(collections).reduce(
-	(sagas, [collectionName, collection]) => {
-		const collectionFile = collection.path;
-		sagas[collectionName] = sagaBuilder(collectionName, require(collectionFile), definitions); // eslint-disable-line
+	(sagas, [sagaName, saga]) => {
+		const sagaFile = saga.path;
+		sagas[sagaName] = sagaBuilder(require(sagaFile), definitions); // eslint-disable-line
 		return sagas;
 	},
 	{},
@@ -16,7 +16,7 @@ const buildSagas = (collections, definitions) => Object.entries(collections).red
 // Domain may be a path to the domain dir or a loaded domain object
 // Definitions come from cqrs-domain module
 module.exports = (sagas, definitions) => {
-	if (typeof collections === 'string' || sagas instanceof String)
+	if (typeof sagas === 'string' || sagas instanceof String)
 		sagas = loader(sagas);
 	return buildSagas(sagas, definitions);
 };
