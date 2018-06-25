a12cf64a11d7cadd0f968ab58b5c0cb3c26a7ff4
Dimitar Nanov <dimitar.nanov@ecollect.net>
2018-06-25 12:41:39 +0300

    fix asyncParamCall

--- utils/index.js
+++ utils/index.js
@@ -18,8 +18,8 @@ const asyncParamCallback = (fn, ...params) => {
 
 const asyncParamApiCallbacks = {
 	1: (fn, api) => (par1, callback) => Promise.resolve(fn(par1, api(par1))).then(result => callback(null, result), error => callback(error)),
-	2: (fn, api) => (par1, par2, callback) => Promise.resolve(fn(par1, par2, api(par1))).then(result => callback(null, result), error => callback(error)),
-	3: (fn, api) => (par1, par2, par3, callback) => Promise.resolve(fn(par1, par2, par3, api(par1))).then(result => callback(null, result), error => callback(error)),
+	2: (fn, api) => (par1, par2, callback) => Promise.resolve(fn(par1, par2, api(par1, par2))).then(result => callback(null, result), error => callback(error)),
+	3: (fn, api) => (par1, par2, par3, callback) => Promise.resolve(fn(par1, par2, par3, api(par1, par2, par3))).then(result => callback(null, result), error => callback(error)),
 };
 
 const asyncParamApiCallback = (fn, api, ...params) => {
@@ -28,7 +28,7 @@ const asyncParamApiCallback = (fn, api, ...params) => {
 	if (!asyncFn)
 		throw new Error(`Async param callback function with ${params.length} parameters is not implemented yet.`);
 
-	return asyncFn(fn);
+	return asyncFn(fn, api);
 };
 
 const noop = () => ({});
