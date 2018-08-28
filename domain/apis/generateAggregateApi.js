5e2ef155ac92f35d386975f0650e20db65b7bc2f
Dimitar Nanov <dimitar.nanov@ecollect.net>
2018-08-28 18:39:50 +0300

    recreate apply logic

--- domain/apis/generateAggregateApi.js
+++ domain/apis/generateAggregateApi.js
@@ -5,13 +5,72 @@ const merge = require('lodash.merge');
 
 const { valueop } = require('../../utils');
 
-const generateEvent = (aggregate, cmd, name, payload, metadata) => {
+const generateEvent = (aggregate, model, eventEnricher, cmd, name, payload, metadata, version = 0) => {
 	const event = {};
+
+	const protoSet = Object.getPrototypeOf(model).set;
+	model.set = (...params) => protoSet.apply(model, params);
+
 	dotty.put(event, aggregate.definitions.event.name, name);
 	dotty.put(event, aggregate.definitions.event.payload, payload);
 	// merge metadata
 	dotty.put(event, aggregate.definitions.event.meta, merge(metadata, dotty.get(cmd, aggregate.definitions.command.meta)));
-	return event;
+
+	const context = aggregate.context.name;
+	const aggregateName = aggregate.name;
+	const aggregateId = model.id;
+
+	// TODO CHECK
+	dotty.put(event, aggregate.definitions.event.aggregateId, aggregateId);
+
+	dotty.put(event, aggregate.definitions.event.correlationId, dotty.get(cmd, aggregate.definitions.command.id));
+
+	// version
+	if (aggregate.definitions.event.version) {
+		if (Number.isInteger(version))
+			dotty.put(event, aggregate.definitions.event.version, version);
+
+		if (!dotty.exists(event, aggregate.definitions.event.version)) {
+			const evtName = dotty.get(event, aggregate.definitions.event.name);
+			const maxVersion = aggregate.getEvents().reduce((res, e) => {
+				if (e.name !== evtName)
+					return res;
+
+				return Math.max(e.version || 0, res);
+			}, 0);
+			dotty.put(event, aggregate.definitions.event.version, maxVersion);
+		}
+	}
+
+	if (aggregate.definitions.event.aggregate)
+		dotty.put(event, aggregate.definitions.event.aggregate, aggregateName);
+
+	if (aggregate.definitions.event.context)
+		dotty.put(event, aggregate.definitions.event.context, context);
+
+	// handle revision
+	if (!aggregate.disablePersistence) {
+		const streamInfo = {
+			context,
+			aggregate: aggregateName,
+			aggregateId,
+		};
+		const revision = model.getRevision(streamInfo) + 1;
+
+		model.setRevision(streamInfo, revision);
+		dotty.put(event, aggregate.definitions.event.revision, revision);
+	}
+
+	const enrichedEvent = eventEnricher(event, model, cmd) || event;
+
+	model.addUncommittedEvent(enrichedEvent);
+	aggregate.apply(enrichedEvent, model);
+
+	model.set = () => {
+		throw Error('Invalid operation on this step.');
+	};
+
+	return enrichedEvent;
 };
 
 const generateAggregateApi = (aggregate, eventEnricher = valueop) => {
@@ -27,8 +86,8 @@ const generateAggregateApi = (aggregate, eventEnricher = valueop) => {
 	};
 
 	AggregateApi.prototype.apply = function apply(name, payload, metadata) {
-		const evt = generateEvent(aggregate, this._command, name, payload, metadata);
-		this._aggregateModel.apply(eventEnricher(evt, this._aggregateModel, this._command) || evt);
+		generateEvent(aggregate, this._aggregateModel, eventEnricher, this._command, name, payload, metadata);
+		// this._aggregateModel.apply(eventEnricher(evt, this._aggregateModel, this._command) || evt);
 	};
 
 	aggregate.events.forEach(({ name }) => {
